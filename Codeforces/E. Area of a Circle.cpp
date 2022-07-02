86710b8c70290ae9b263eac16e3c6df27964a2e1
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-07-02 14:04:00 +0600

    Day 01 - Chap 01 - Contest 01 - PA to PG

---
+++ Codeforces/E. Area of a Circle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   double r;
+   cin >> r;
+   cout << fixed << setprecision(9);
+   cout << r*r*3.141592653;
+
+   return 0;
+}
---
+++ Codeforces/G. Summation from 1 to N.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   unsigned long long N, i, r=0; cin >> N;
+   cout << (N*(N+1))/2;
+
+   return 0;
+}
