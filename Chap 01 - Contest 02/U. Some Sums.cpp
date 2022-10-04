9789c00c028db0310bdabe31e7a0847c71980257
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-10-05 05:48:32 +0600

    a bit tricky

--- Chap 01 - Contest 02/U. Some Sums.cpp
+++ Chap 01 - Contest 02/U. Some Sums.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+   int n, a, b, sumofDigits[2], result=0;
+   cin >> n >> a >> b;
+
+   for(int i=a; i<n; i++) {
+      sumofDigits[0] = i%10;
+      sumofDigits[1] = i/10;
+
+      if(sumofDigits[0] + sumofDigits[1] >= a and sumofDigits[0] + sumofDigits[1] <= b) {
+         result += i;
+         cout << result << " ";
+      }
+   }
+   cout << result;
+
+   return 0;
+}
