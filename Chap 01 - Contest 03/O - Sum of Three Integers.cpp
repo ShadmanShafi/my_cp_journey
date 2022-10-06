92346e651b8bf0b122157b76fc44416e08062ae9
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-10-07 00:35:38 +0600

    a bit tricky

--- Chap 01 - Contest 03/O - Sum of Three Integers.cpp
+++ Chap 01 - Contest 03/O - Sum of Three Integers.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
+   int k, s, count=0;
+   cin >> k >> s;
 
+   for(int i=0; i<=k; i++) {
+      for(int j=0; j<=k; j++) {
+         if(s-i-j >= 0 and s-i-j <= k) {
+            count++;
+         }
+
+      }
+   }
+   cout << count;
 
    return 0;
 }
